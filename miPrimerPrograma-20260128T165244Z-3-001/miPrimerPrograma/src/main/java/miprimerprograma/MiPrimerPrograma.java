a9bc2e863da5ca054af6f0e3ad3e0880df0e1384
anng-mndz <lmendezf1@miumg.edu.gt>
2026-01-28 11:01:37 -0600

    Mi primer programa

---
+++ miPrimerPrograma-20260128T165244Z-3-001/miPrimerPrograma/src/main/java/miprimerprograma/MiPrimerPrograma.java
@@ -0,0 +1,18 @@
+/*
+//Luis Angel Méndez Fuentes
+//9959-24-6845
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimerprograma;
+
+/**
+ *
+ * @author daguilae
+ */
+public class MiPrimerPrograma {
+        public static void main(String[] args) {
+            System.out.println("Hola mundo");
+    }
+}
